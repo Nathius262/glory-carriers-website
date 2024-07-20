9bf4d1de96d4d8d09a0c07d83e6c96a78f62a614
nathius262 <egbodonathaniel@yahoo.com>
2024-07-20 09:26:27 +0100

    modified index errorhandler

---
+++ index.js
@@ -0,0 +1,44 @@
+//main import
+import express from 'express';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+//custom import
+import {internalServerError, pageNotFound} from './middlewares/errorHandler.js'
+import staticFiles from "./config/staticFiles.js"
+import hbs from "./config/settings.js"
+import rootRouter from "./routers/rootRouter.js"
+import mediaRouter from "./routers/mediaRouter.js"
+
+
+dotenv.config()
+
+// Derive the equivalent of __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+//settings
+app.engine('html', hbs.engine);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+
+//staticfiles
+app.use(staticFiles);
+
+// routes
+app.use('/', rootRouter);
+app.use('/media/', mediaRouter);
+
+//middlewares
+app.use(internalServerError);
+app.use(pageNotFound);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,10 +1,10 @@
 const pageNotFound = (req, res, next) => {
-    res.status(404).render('./error/404', { url: `${req.protocol}://${req.get('host')}${req.originalUrl}` });
+    res.status(404).render('./errors/404', { url: `${req.protocol}://${req.get('host')}${req.originalUrl}` });
 };
 
 const internalServerError = (err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).render('./error/500', { message: 'Internal Server Error', error: err.message });
+    res.status(500).render('./errors/500', { message: 'Internal Server Error', error: err.message });
 };
 
 export {internalServerError, pageNotFound}
