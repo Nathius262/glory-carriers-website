be289c28e4711ed9d2ec2e14535c85ab6c15c267
Nathius262 <egbodonathaniel@yahoo.com>
2024-08-19 14:54:25 +0100

    modified static config

--- config/staticFiles.js
+++ config/staticFiles.js
@@ -12,6 +12,7 @@ const app = express();
 
 // Serve static files (CSS, JS, images)
 app.use('', express.static(path.join(__dirname, '..', 'public')));
+app.use('/auth', express.static(path.join(__dirname, '..', 'public')));
 app.use('/media', express.static(path.join(__dirname, '..', 'public')));
 app.use('/media/sermon', express.static(path.join(__dirname, '..', 'public')));
 
