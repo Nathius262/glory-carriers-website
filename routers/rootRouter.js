aa1615328805a534f9e22c28ca4998acbec1033f
Nathius262 <egbodonathaniel@yahoo.com>
2024-10-06 21:34:46 +0100

    updated online registration

--- routers/rootRouter.js
+++ routers/rootRouter.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { renderIndex, renderAbout, renderSitemap, renderDepartment, renderContact, renderEvent, renderGiving } from "../controllers/rootController.js";
 import {renderSearch} from '../controllers/mediaController.js'
+import {registerOnlineUser, onlineRegisterForm} from '../controllers/authController.js'
 
 const router = Router();
 
@@ -13,6 +14,11 @@ router.get('/event', renderEvent);
 router.get('/giving', renderGiving);
 router.get('/search', renderSearch);
 router.get('/sitemap.xml', renderSitemap);
+
+
+router.route('/members')
+    .get(onlineRegisterForm)
+    .post(registerOnlineUser);
 
 
 export default router;
