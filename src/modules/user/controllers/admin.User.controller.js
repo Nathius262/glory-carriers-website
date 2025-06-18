5961800150eb6fbf29dab5a4bcbfa41f025c2242
Nathaniel <egbodonathaniel@yahoo.com>
2025-06-19 00:17:41 +0100

    update user admin controller

--- src/modules/user/controllers/admin.User.controller.js
+++ src/modules/user/controllers/admin.User.controller.js
@@ -1,39 +1,71 @@
 import * as service from '../services/admin.User.service.js';
+import { check, validationResult, body } from 'express-validator';
+
 
 export const findAll = async (req, res) => {
   try {
-    const data = await service.findAll();
-    res.status(200).render('./admins/list', {
-      success: true,
-      pageTitle: "Admin",
-      users: data,
+    const { page, limit, offset } = req.pagination;
+
+    const { data, totalPages } = await service.findAll({ limit, offset });
+
+    res.render('admins/user_list', {
+      users:data,
+      currentPage: page,
+      totalPages,
+      pageTitle: 'Admin',
     });
   } catch (err) {
-    res.status(500).render('error', { error: err.message });
+    res.status(500).render('errors/500', { error: err.message });
   }
 };
+
 
 export const findById = async (req, res) => {
   try {
     const data = await service.findById(req.params.id);
-    res.status(200).render('./admins/update', {
+    res.status(200).render('admins/user_update', {
       success: true,
       pageTitle: "Update Record",
-      user: [data],
+      user: data
     });
   } catch (err) {
-    res.status(404).render('error', { error: err.message });
+    res.status(404).render('errors/500', { error: err.message });
   }
 };
 
-export const create = async (req, res) => {
-  try {
-    const data = await service.create(req.body);
-    res.status(201).json({ success: true, data });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  export const create = [
+    
+    [
+    check('email', 'Please include a valid email').isEmail(),
+    check('username', 'Username is required').not().isEmpty(),
+    check('password', 'Please enter a password with 8 or more characters').isLength({ min: 8 }),
+    check('is_admin', 'is_admin should be a boolean').optional().isBoolean(),
+    check('is_staff', 'is_staff should be a boolean').optional().isBoolean(),
+
+
+    // Custom validation to check if confirmPassword matches password
+    body('confirmPassword').custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords do not match');
+      }
+      return true;
+    }),
+  ],
+    async (req, res) => {
+      const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    
+    try {
+      const data = await service.create(req.body);
+      res.status(201).json({ success: true, data });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
   }
-};
+]
 
 export const update = async (req, res) => {
   try {
@@ -55,10 +87,10 @@ export const destroy = async (req, res) => {
 
 export const renderCreate = async (req, res) => {
   try {
-    res.status(200).render('./admins/create', {
+    res.status(200).render('admins/user_create', {
       pageTitle: "Create User"
     });
   } catch (err) {
-    res.status(500).render('error', { error: err.message });
+    res.status(500).render('errors/500', { error: err.message });
   }
 };
