e0b5147d54e8ea2236e3aee54cfd7ca927aa4cc4
Nathaniel <egbodonathaniel@yahoo.com>
2025-06-19 00:07:43 +0100

    create user module

---
+++ src/modules/user/routes/User.routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import userModuleViews from '../../../middlewares/moduleViews.js';
+import * as controller from '../controllers/User.controller.js';
+
+const router = express.Router();
+
+router.use(useModuleViews('user'));
+
+// Public view routes
+router.get('/', controller.findAll);
+router.get('/:id', controller.findById);
+
+export default router;
