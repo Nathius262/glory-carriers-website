b954486518a3b5347f3acae0b6166b9f4ed439e0
Nathaniel <egbodonathaniel@yahoo.com>
2025-06-20 16:15:22 +0100

    update controller

--- src/modules/media/controllers/Nowword.controller.js
+++ src/modules/media/controllers/Nowword.controller.js
@@ -1,27 +1,32 @@
 import * as service from '../services/Nowword.service.js';
 
 export const findAll = async (req, res) => {
+  const {page, limit, offset} = req.pagination
   try {
-    const data = await service.findAll();
-    res.status(200).render('./list', {
+    const data = await service.findAll({limit, offset});
+    res.status(200).render('./nowword/nowword_list', {
       success: true,
-      pageTitle: "",
-      nowwords: data,
+      pageTitle: "Nowword List",
+      nowwords: data.nowwords,
+      totalItems: data.totalItems,
+      totalPages: data.totalPages,
+      currentPage: page
     });
   } catch (err) {
-    res.status(500).render('error', { error: err.message });
+    console.log(err)
+    res.status(500).render('errors/500', { error: err.message });
   }
 };
 
 export const findById = async (req, res) => {
   try {
     const data = await service.findById(req.params.id);
-    res.status(200).render('./single', {
+    res.status(200).render('./nowword/nowword_detail', {
       success: true,
-      pageTitle: "Details",
-      nowword: [data],
+      pageTitle: "Nowword Detail",
+      nowword: data,
     });
   } catch (err) {
-    res.status(404).render('error', { error: err.message });
+    res.status(404).render('errors/404', { error: err.message });
   }
 };
