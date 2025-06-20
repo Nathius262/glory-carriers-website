8441b9bdef04e282fd8682d3eb9ad7f614817d1f
Nathaniel <egbodonathaniel@yahoo.com>
2025-06-20 23:20:39 +0100

    create event module

---
+++ src/modules/event/migrations/20250620220919-create-event.cjs
@@ -0,0 +1,47 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      start_date: {
+        type: Sequelize.DATE,
+        allowNull:false
+      },
+      end_date: {
+        type: Sequelize.DATE
+      },
+      is_recurring:{
+        type:Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      is_headline:{
+        type:Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('events');
+  }
+};
---
+++ src/modules/event/models/event.cjs
@@ -0,0 +1,45 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Event extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Event.init({
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      description: {
+        type: DataTypes.TEXT
+      },
+      start_date: {
+        type: DataTypes.DATE,
+        allowNull:false
+      },
+      end_date: {
+        type: DataTypes.DATE
+      },
+      is_recurring:{
+        type:DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+      is_headline:{
+        type:DataTypes.BOOLEAN,
+        defaultValue: false
+      }
+  }, {
+    sequelize,
+    modelName: 'Event',
+    tableName: 'events'
+  });
+  return Event;
+};
