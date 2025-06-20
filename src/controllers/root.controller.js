13cf5e87c97dea021378bfbe6d8181be90a533ad
Nathaniel <egbodonathaniel@yahoo.com>
2025-06-20 14:37:10 +0100

    update home page

--- src/controllers/root.controller.js
+++ src/controllers/root.controller.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import * as sermonService from '../modules/sermon/services/Sermon.service.js';
 
 // Derive the equivalent of __dirname
 import { fileURLToPath } from 'url';
@@ -15,11 +16,12 @@ const page_logo = process.env.PAGELOGO
 const renderIndex= async (req, res) => {
     try {
 
-
+        const sermons = await sermonService.findAll({limit:2, offset:0});
         //console.log(result.rows)
         res.render('index', {
             pageTitle: "Home",
-            pageLogo: page_logo
+            pageLogo: page_logo,
+            sermons: sermons.sermons
         });
     } catch (err) {
         res.status(500).render('./errors/500', { message: 'Internal Server Error', error: err.message });
