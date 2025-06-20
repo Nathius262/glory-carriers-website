5a64e60fdf7eaeb2451cb9101ef36a81e28c6c50
Nathaniel <egbodonathaniel@yahoo.com>
2025-06-20 16:28:04 +0100

    update model name

--- src/modules/media/models/nowword.cjs
+++ src/modules/media/models/nowword.cjs
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     file_url: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'nowword',
+    modelName: 'Nowword',
   });
   return nowword;
 };
