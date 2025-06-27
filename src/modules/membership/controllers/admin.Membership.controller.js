1fb84c2b58ad45d6edddf9c1130be33adde46661
Nathaniel <egbodonathaniel@yahoo.com>
2025-06-27 17:58:03 +0100

    register admin to membership module

---
+++ src/modules/membership/controllers/admin.Membership.controller.js
@@ -0,0 +1,74 @@
+import * as service from '../services/admin.Membership.service.js';
+
+export const findAll = async (req, res) => {
+  const {page, limit, offset} = req.pagination
+  try {
+    const data = await service.findAll({limit, offset});
+    res.status(200).render('./admins/membership_list', {
+      success: true,
+      pageTitle: "Admin",
+      memberships: data.memberships,
+      totalItems: data.totalItems,
+      totalPages: data.totalPages,
+      currentPage: page
+    });
+  } catch (err) {
+  console.log(err)
+    res.status(500).render('errors/500', { error: err });
+  }
+};
+
+export const findById = async (req, res) => {
+  try {
+    const data = await service.findById(req.params.id);
+    res.status(200).render('./admins/membership_update', {
+      success: true,
+      pageTitle: "Update Record",
+      membership: data,
+    });
+  } catch (err) {
+  console.log(err)
+    res.status(404).render('errors/404', { error: err });
+  }
+};
+
+export const create = async (req, res) => {
+  try {
+    const data = await service.create(req.body);
+    res.status(201).json({ success: true, data });
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: err });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const data = await service.update(req.params.id, req.body);
+    res.status(200).json({ success: true, data });
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: err });
+  }
+};
+
+export const destroy = async (req, res) => {
+  try {
+    const data = await service.destroy(req.params.id);
+    res.status(200).json({ success: true, message: 'Deleted successfully', data });
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: err });
+  }
+};
+
+export const renderCreate = async (req, res) => {
+  try {
+    res.status(200).render('./admins/membership_create', {
+      pageTitle: "Create Membership"
+    });
+  } catch (err) {
+    console.log(err)
+    res.status(500).render('errors/500', { error: err });
+  }
+};
---
+++ src/modules/membership/routes/admin.Membership.routes.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import useModuleViews from '../../../middlewares/moduleViews.js';
+import {withPagination} from '../../../middlewares/paginations.js';
+import * as controller from '../controllers/admin.Membership.controller.js';
+
+const router = express.Router();
+
+router.use(useModuleViews('membership'));
+
+// Admin view routes
+router.route('/')
+  .get(withPagination(10), controller.findAll)
+  .post(controller.create);
+
+router.get('/create', controller.renderCreate);
+
+router.route('/:id')
+  .get(controller.findById)
+  .put(controller.update)
+  .delete(controller.destroy);
+
+export default router;
