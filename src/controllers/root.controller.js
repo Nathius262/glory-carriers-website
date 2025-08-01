5500f5f995af3e298ac5d2643e77768247342122
Nathaniel Egbodo Odo <nathaniel.egbodo@ignition633.org>
2025-08-01 18:19:03 +0100

    add privacy policy

--- src/controllers/root.controller.js
+++ src/controllers/root.controller.js
@@ -54,3 +54,14 @@ export const ads_txt_view = async (req, res) => {
         res.status(404).send('Not found');
     }
 }
+
+export const privacy_policy_view = async (req, res) => {
+    try {
+        res.render('others/privacy_policy', {
+            pageTitle: "Privacy Policy",
+            pageLogo: page_logo,
+        });
+    } catch (error) {
+        res.status(500).render('./errors/500', { message: 'Internal Server Error', error: err.message });
+    }
+}
--- src/routes/root.routes.js
+++ src/routes/root.routes.js
@@ -9,6 +9,7 @@ router.get('/', controller.index_view);
 router.get('/about', controller.about_view);
 router.get('/sitemap', controller.sitemap_view);
 router.get('/ads.txt', controller.ads_txt_view);
+router.get('/privacy-policy', controller.privacy_policy_view);
 router.get('/admin', dashboard_view)
 
 
