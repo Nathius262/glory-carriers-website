dbf6ac0b5c47fc7a60b2672a685d3cc55a956b83
Nathaniel Egbodo Odo <egbodonathaniel@yahoo.com>
2026-04-12 13:30:45 +0100

    feat: update sermon limit to 6 and adjust form styles across authentication views

--- src/modules/sermon/routes/Sermon.routes.js
+++ src/modules/sermon/routes/Sermon.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import useModuleViews from '../../../middlewares/moduleViews.js';
-import {withPagination} from '../../../middlewares/paginations.js';
+import { withPagination } from '../../../middlewares/paginations.js';
 import * as controller from '../controllers/Sermon.controller.js';
 
 const router = express.Router();
@@ -8,7 +8,7 @@ const router = express.Router();
 router.use(useModuleViews('sermon'));
 
 // Public view routes
-router.get('/', withPagination(10), controller.findAll);
+router.get('/', withPagination(40), controller.findAll);
 router.get('/:id', controller.findById);
 
 export default router;
