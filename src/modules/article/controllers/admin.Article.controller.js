1f539ad9c12eee909f53e1729f7ee1e0da34e078
Nathaniel Egbodo Odo <egbodonathaniel@yahoo.com>
2026-07-15 10:40:39 -0400

    feat(article): implement CRUD functionality and views for article management

---
+++ src/modules/article/controllers/admin.Article.controller.js
@@ -0,0 +1,80 @@
+import * as service from '../services/admin.Article.service.js';
+
+export const findAll = async (req, res) => {
+  const {page, limit, offset} = req.pagination
+  try {
+    const data = await service.findAll({limit, offset});
+    res.status(200).render('./admins/article_list', {
+      success: true,
+      pageTitle: "Admin",
+      layout: "admin", 
+      PageTitle: "Admin",
+      articles: data.articles,
+      totalItems: data.totalItems,
+      totalPages: data.totalPages,
+      currentPage: page
+    });
+  } catch (err) {
+  console.log(err)
+    res.status(500).render('errors/500', { error: err });
+  }
+};
+
+export const findById = async (req, res) => {
+  try {
+    const data = await service.findById(req.params.id);
+    res.status(200).render('./admins/article_update', {
+      success: true,
+      pageTitle: "Update Record",
+      layout: "admin", 
+      PageTitle: "Admin",
+      article: data,
+    });
+  } catch (err) {
+  console.log(err)
+    res.status(404).render('errors/404', { error: err });
+  }
+};
+
+export const create = async (req, res) => {
+  try {
+    const data = await service.create(req.body);
+    res.status(201).json({ success: true, redirectTo: "/admin/article", message: "Created successfully" });
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: err });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const data = await service.update(req.params.id, req.body);
+    res.status(200).json({ success: true, data, redirectTo: `/admin/article/${req.params.id}`, message: "Updated successfully" });
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: err });
+  }
+};
+
+export const destroy = async (req, res) => {
+  try {
+    const data = await service.destroy(req.params.id);
+    res.status(200).json({ success: true, message: 'Deleted successfully', redirectTo: "/admin/article" });
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: err });
+  }
+};
+
+export const renderCreate = async (req, res) => {
+  try {
+    res.status(200).render('./admins/article_create', {
+      pageTitle: "Create Article", 
+      layout: "admin", 
+      PageTitle: "Admin"
+    });
+  } catch (err) {
+    console.log(err)
+    res.status(500).render('errors/500', { error: err });
+  }
+};
